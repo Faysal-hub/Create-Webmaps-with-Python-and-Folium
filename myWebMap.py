33e217b09ebb8cf0776bcd13c9569ba849d08ae0
Faysal-hub <ovitahsan@gmail.com>
2020-03-21 21:49:25 +0100

    Adding Points

--- myWebMap.py
+++ myWebMap.py
@@ -1,4 +1,10 @@
 import folium
-map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
-map.save("map1.html")
 
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
+
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi, I am a Marker", icon=folium.Icon(color='red')))
+
+
+map.add_child(fg)
+map.save("map1.html")
