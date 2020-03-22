1dabbd34dfbc59a59900f124cea525087900e27f
Faysal-hub <ovitahsan@gmail.com>
2020-03-22 08:46:45 +0100

    color points

--- myWebMap.py
+++ myWebMap.py
@@ -5,15 +5,25 @@ data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
+name = list(data["NAME"])
 
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
 
-for lt,ln, el in zip (lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color='red')))
+fgv = folium.FeatureGroup(name="Volcanoes")
 
+for lt, ln, el in zip(lat, lon, elev):
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+" m",
+    fill_color=color_producer(el), fill=True,  color = 'grey', fill_opacity=0.7))
 
-map.add_child(fg)
+map.add_child(fgv)
 map.save("map1.html")
